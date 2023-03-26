9ee566950dda18508c98fe8f5e8567e2893a3137
milliorn <scottmilliorn@gmail.com>
2023-03-25 20:09:02 -0700

    post to element(will post to route later)

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,54 @@
 import { useState } from "react";
 
 function App(): JSX.Element {
-  // Declare state variables for the selected file and whether a file has been picked
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  // state variables for the selected file, whether a file is picked, and the contents of the file
+  const [fileContents, setFileContents] = useState<string>("");
   const [isFilePicked, setIsFilePicked] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  // Event handler for file selection input change
+  // function to handle when the user selects a file
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    // Update state with the selected file and set isFilePicked to true
-    setSelectedFile(event.target.files![0]);
-    setIsFilePicked(true);
+    setIsFilePicked(true); // set that a file has been picked
+    setSelectedFile(event.target.files![0]); // set the selected file
   };
 
-  // Event handler for form submission
+  // function to handle when the user submits the form
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
+
+    // read the contents of the selected file
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setFileContents(reader.result as string);
+      };
+      reader.readAsText(selectedFile);
+    }
+
     console.log("Selected file:", selectedFile);
-    // handle file upload logic here
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-800 text-white">
-      {/* Heading */}
       <h1 className="text-3xl font-bold underline text-center">Upload File</h1>
-
-      {/* Form */}
+      {/* form to upload a file */}
       <form className="mt-8 flex flex-col items-center" onSubmit={handleSubmit}>
-        {/* File upload input */}
         <div className="mb-4">
           <label
             htmlFor="file-upload"
             className="cursor-pointer bg-blue-200 hover:bg-blue-400 rounded font-bold py-2 px-4 flex flex-col max-w-xs items-center justify-center mx-auto my-0"
           >
-            {/* Display "Select a file" if no file has been picked yet, otherwise display the selected file name */}
             <span className="text-lg font-bold text-center text-black">
+              {/* show the name of the selected file, or prompt to select a file */}
               {selectedFile ? selectedFile.name : "Select a file"}
             </span>
           </label>
-
           {isFilePicked && (
             <span className="text-lg font-bold flex items-center mx-auto my-0 pt-4 pb-2 px-4 text-center">
+              {/* prompt the user to upload the selected file */}
               Press Upload or click on {selectedFile?.name}
             </span>
           )}
-
-          {/* Actual file input, hidden */}
           <input
             id="file-upload"
             name="file-upload"
@@ -53,8 +57,6 @@ function App(): JSX.Element {
             className="hidden"
           />
         </div>
-
-        {/* Submit button */}
         <button
           type="submit"
           className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
@@ -62,6 +64,12 @@ function App(): JSX.Element {
           Upload
         </button>
       </form>
+      {/* display the contents of the uploaded file */}
+      {fileContents && (
+        <div className="mt-8">
+          <p id="file-contents">{fileContents}</p>
+        </div>
+      )}
     </div>
   );
 }
